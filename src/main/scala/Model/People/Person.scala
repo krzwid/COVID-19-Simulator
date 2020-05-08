4b58bb5c47682a7c54bfc58a5349aabeabb550fe
MrPaulMarshall <45012218+MrPaulMarshall@users.noreply.github.com>
2020-05-08 17:09:12 +0200

    test commit

--- src/main/scala/Model/People/Person.scala
+++ src/main/scala/Model/People/Person.scala
@@ -3,8 +3,6 @@ package Model.People
 import Model.MapSites.Room
 
 trait Person {
-//  def getFirstname(): String
-//  def getLastname(): String
   def getID: Int
   def isInfected: Boolean
   def infectedSince: Int
