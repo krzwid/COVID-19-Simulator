f9dc18c14a167c14c4896db0c416fc569b358d43
speedy <k.w1@o2.pl>
2020-05-14 13:47:15 +0200

    Update DailyData

--- src/main/scala/Model/Statistics/DailyData.scala
+++ src/main/scala/Model/Statistics/DailyData.scala
@@ -1,25 +1,44 @@
 package Model.Statistics
 
 // graphs to draw for each day:
-// 1) infected
-// 2) dead
-// 3) dead (with other diseases)
-// 4) alive staff
-// 5) alive patients
+
+//1) infected covid
+//2) dead covid
+//3) dead other
+//4) dead all
+//5) cured covid
+//6) active covid
+//7) alive patients
+//8) alive staff
+//9) mortality
 
 class DailyData {
-  private var countDead: Int = 0
-  private var countDeadStaff: Int = 0
-  private var countDeadNonStaff: Int = 0
+  private val countDeadAll: Int = countDeadCovid + countDeadOther
+  private val countDeadPatientAll: Int = countDeadPatientCovid + countDeadPatientCovid
+  private val countDeadStaffAll: Int = countDeadStaffCovid + countDeadStaffOther
 
-  private var countNewInfected: Int = 0
-  private var countNewInfectedStaff: Int = 0
-  private var countNewInfectedNonStaff: Int = 0
+  private val countDeadCovid: Int = countDeadStaffCovid + countDeadPatientCovid
+  private var countDeadStaffCovid: Int = 0
+  private var countDeadPatientCovid: Int = 0
 
-  private var countCured: Int = 0
+  private val countDeadOther: Int = countDeadStaffOther + countDeadPatientOther
+  private var countDeadStaffOther: Int = 0
+  private var countDeadPatientOther: Int = 0
+
+  private var countNewInfected: Int = countNewInfectedStaffCovid + countNewInfectedPatientCovid
+  private var countNewInfectedStaffCovid: Int = 0
+  private var countNewInfectedPatientCovid: Int = 0
+
+  private val countCured: Int = countCuredStaff + countCuredPatient
   private var countCuredStaff: Int = 0
-  private var countCuredNonStaff: Int = 0
+  private var countCuredPatient: Int = 0
 
-  private var newStaff: Int = 0
+  private val activeCovid: Int = activeStaffCovid + activePatientCovid
+  private var activePatientCovid: Int = 0
+  private var activeStaffCovid: Int = 0
 
+  private var countPatient: Int = 0
+  private var countStaff: Int = 0
+
+  private val mortality: Double = countDeadCovid / (countPatient)
 }
